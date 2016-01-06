4e0c9e96dc8294961bdd83364a29304bdf540979
O4epegb <O4epegb@mail.ru>
2016-01-06 17:02:39 +0300

    Added redis

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var urlencode = bodyParser.urlencoded( {extended: false} );
+
+// Redis connection
+var redis = require('redis');
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+    var client = redis.createClient();
+}
+
+client.select((process.env.NODE_ENV || 'development').length);
+// end redis connection
+
 app.use(express.static('public'));
 
 app.get('/', function (request, response) {
@@ -8,8 +24,17 @@ app.get('/', function (request, response) {
 });
 
 app.get('/cities', function (request, response) {
-  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
-  response.json(cities);
+  client.hkeys('cities', function (error, names) {
+    response.json(names);
+  })
+});
+
+app.post('/cities', urlencode, function (request, response) {
+  var newCity = request.body;
+  client.hset('cities', newCity.name, newCity.description, function (error) {
+    if (error) throw error;
+    response.status(201).json(newCity.name);
+  });
 });
 
 module.exports = app;
